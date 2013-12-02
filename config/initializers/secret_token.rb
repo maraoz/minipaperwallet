c50203618441f35405ea5bf0c501bbfbd115d309
Manuel Aráoz <manuelaraoz@gmail.com>
2013-12-02 17:41:47 -0300

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minipaperwallet::Application.config.secret_key_base = '9c88eb9492cffdf3a81b6b8898fbb73c6917345e428a97dd5503249667e895ecc6544030b41bacbeb3d13987c9d7e7cd4b5480094cea370fbbc9d74a9639308e'
